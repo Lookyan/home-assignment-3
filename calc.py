50cd83938868c462bc95c55e441c399380af2f3b
Alex <digwnews@gmail.com>
2015-11-14 17:51:49 +0300

    refactor to decorator

--- calc.py
+++ calc.py
@@ -1,27 +1,39 @@
 import math
 from decimal import *
 
+def set_precision(func):
+        def nfunc(*args):
+            getcontext().prec = 10
+            return float(func(*args))
+        return nfunc
 
 class Calc:
     def __init__(self):
         getcontext().prec = 10
 
     @staticmethod
+    @set_precision
     def add(x, y):
-        return float(Decimal(x) + Decimal(y))
+        return Decimal(x) + Decimal(y)
 
     @staticmethod
+    @set_precision
     def sub(x, y):
-        return float(Decimal(x) - Decimal(y))
+        return Decimal(x) - Decimal(y)
 
     @staticmethod
+    @set_precision
     def mul(x, y):
-        return float(Decimal(x) * Decimal(y))
+        return Decimal(x) * Decimal(y)
 
     @staticmethod
+    @set_precision
     def div(x, y):
-        return float(Decimal(x) / Decimal(y))
+        return Decimal(x) / Decimal(y)
 
     @staticmethod
+    @set_precision
     def root(x, y):
-        return float(math.pow(x, float(Decimal(1) / Decimal(y))))
+        if y < 1 or x < 0:
+            raise ValueError
+        return Decimal(x) ** (Decimal(1) / Decimal(int(y)))
